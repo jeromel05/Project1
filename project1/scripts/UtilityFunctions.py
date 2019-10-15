c54b5b4263d18e9efc0c226e9dcee39eb6bbcf26
Anne-Aimee <anne-aimee.bernard@epfl.ch>
2019-10-15 08:57:41 +0200

    oueeee

--- project1/scripts/UtilityFunctions.py
+++ project1/scripts/UtilityFunctions.py
@@ -3,6 +3,7 @@
 """some utility functions for project 1."""
 import numpy as np
 
+#LOSS
 def compute_loss(y, tx, w, method="mse"):
     """Calculate the loss.
 
@@ -15,91 +16,35 @@ def compute_loss(y, tx, w, method="mse"):
         return np.inner(error,error) / np.shape(y)[0] / 2 #for MSE
     else:
         raise Exception("spam")
+
+# SPLIT DATA TRAIN + TEST
+# a utiliser pour les patterns ou on a des mauvaises approximations à chaque fois
+def split_data(x, y, ratio, seed=1):
+    # split the data based on the given ratio
+    """
+    split the dataset based on the split ratio. If ratio is 0.8 
+    you will have 80% of your data set dedicated to training 
+    and the rest dedicated to testing
+    """
+    # set seed
+    np.random.seed(seed)
+    # ***************************************************
+    itrain=np.random.choice(y.shape[0],int(ratio*y.shape[0]),replace=False)
+    # np.arange(size) donne les numéros de 0 à size
+    itest = np.delete(np.arange(y.shape[0]),itrain)
+    xtrain=x[itrain]
+    ytrain=y[itrain]
+    xtest=x[itest]
+    ytest=y[itest]
+    return xtrain,ytrain,xtest,ytest
     
+    
+#FOR GRADIENT DESCENT   
 def compute_gradient(y, tx, w):
     """Compute the gradient."""
     n = np.shape(tx)[0]
     error = y - np.dot(tx,w)
-    return - np.dot(tx.T,error)/n
-
-
-def leastsquaresGD(y, tx, initial_w, max_iters, gamma):
-    """Gradient descent algorithm with mse."""
-    # Define parameters to store w and loss
-    loss = 0
-    w = initial_w
-    for n_iter in range(max_iters):
-        # ***************************************************
-        grad = compute_gradient(y, tx, w)
-        loss = compute_loss(y, tx, w, method="mse")
-        w = w - gamma*grad
-        # ***************************************************
-        if not(n_iter%30):
-            print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-
-    return loss, w
-
-def standardize(x):
-    """Standardize the original data set."""
-    mean_x = np.mean(x)
-    x = x - mean_x
-    std_x = np.std(x)
-    x = x / std_x
-    return x, mean_x, std_x
-
-
-def build_model_data(height, weight):
-    """Form (y,tX) to get regression data in matrix form 
-    with x[:,0] = 1 (offset)."""
-    y = weight
-    x = height
-    num_samples = len(y)
-    tx = np.c_[np.ones(num_samples), x]
-    return y, tx
-
-def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
-    """
-    Generate a minibatch iterator for a dataset.
-    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
-    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
-    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
-    Example of use :
-    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
-        <DO-SOMETHING>
-    """
-    data_size = len(y)
-
-    if shuffle:
-        shuffle_indices = np.random.permutation(np.arange(data_size))
-        shuffled_y = y[shuffle_indices]
-        shuffled_tx = tx[shuffle_indices]
-    else:
-        shuffled_y = y
-        shuffled_tx = tx
-    for batch_num in range(num_batches):
-        start_index = batch_num * batch_size
-        end_index = min((batch_num + 1) * batch_size, data_size)
-        if start_index != end_index:
-            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
-            
-def leastsquaresSGD(y, tx, initial_w, max_iters, gamma, batch_size=1):
-    """Stochastic gradient descent algorithm."""
-    # ***************************************************
-    grad = 0
-    loss = 0
-    w = initial_w
-    for n_iter in range(max_iters):
-        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
-                grad = compute_gradient(minibatch_y, minibatch_tx,w)
-        grad = grad / batch_size    
-        w = w - grad * gamma
-        loss = compute_loss(y, tx, w)
-        if not(n_iter%10):
-            print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-                  bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-    # ***************************************************
-    return loss, w
+    return - np.dot(tx.T,error)/n          
 
 def compute_subgradient(y, tx, w):
     """Compute the subgradient for MAE."""
@@ -160,8 +105,103 @@ def stochastic_subgradient_descent(
     return losses[-1], ws[-1]
 
 
-# In[ ]:
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
 
 
+# VISUALIZATION
+
+def pca(tX): # Computes principal components of the set of observations.
+    
+    """
+        This orthogonal transformation leads to the first principal component having the largest possible variance.
+        Each succeeding component in turn has the highest variance possible under the constraint that it is orthogonal to the preceding components.
+    """
+
+    n, m = tX.shape
+    # center data matrix tX
+    tX_centered = np.nan_to_num((tX - np.mean(tX, 0)) / np.std(tX, 0)) ## Better solution required!!
+    
+    assert np.allclose(tX_centered.mean(axis=0), np.zeros(m))
+
+    # Compute covariance matrix
+    C = np.dot(tX_centered.T, tX_centered) / (n-1)
+    
+    # Eigen decomposition
+    eigen_vals, eigen_vecs = np.linalg.eig(C)
+    
+    # Project X onto PC space
+    tX_pca = np.dot(tX_centered, eigen_vecs)
+    
+    return tX_pca
+
+
+# FOR LOGISTIC REGRESSION
+
+def compute_p(w,tX): 
+    """
+        Computes probabilities of all observations in tX corresponding to -1 = 'b' or 1 = 's' based on weights w according to the logistc transformation.
+        (p = 0 correponds to -1 resp. 'b' and p = 1 to 1 resp. 's' to simplify the computations)
+    """
+    #odds = np.exp(np.dot(tX,w))
+    odds = np.nan_to_num(np.exp(np.dot(tX,w)))
+    
+    return odds/(1+odds)
+
+def compute_log_likelihood(w,tX, y): # Be careful! y must be binary. (0 or 1) !!
+    "Computes the log-likelihood of observing the data tX with the given weights w."
+    
+    p = compute_p(w,tX)
+    return sum(y*np.nan_to_num(np.log(p))+(1-y)*np.nan_to_num(np.log(1-p)))
+
+def compute_gradient_log_likelihood(w,tX, y): # Be careful! y must be binary. (0 or 1) !!
+    "Computes the gradient of the log-likelihood the given current weights w and the data tX with respect to w."
+    
+    p = compute_p(w,tX) 
+    odds = np.exp(np.dot(tX,w)) 
+    return np.dot(tX.T,(y-p))
+
+def compute_jacobean_log_likelihood(w,tX, y): # Be careful! y must be binary. (0 or 1) !!
+    "Computes the jacobean of the log-likelihood the given current weights w and the data tX with respect to w."
+    
+    jacobean = np.zeros((len(w), len(w)))
+    p = compute_p(w,tX) 
+    for i in range(len(w)):
+        for j in range(len(w)):
+            jacobean[i,j] = sum(tX[:,i]*tX[:,j]*(p*(1-p)))
+    
+    return -jacobean
+
+    #return -np.dot(tx.T,np.dot(np.diag(p*(1-p)),tx)) # A way more elegant solution. Unfortunately, the diagonal matrix is too large.
+
+def rescale_y(y): #rescale y to get estimates between -1 and 1
+    y_rescaled = np.ones(len(y))
+    y_rescaled[np.where(y==-1)] = 0
+    return y_rescaled
+
+def rescale_predictions(p): #reverse rescaling
+    return 2*p-1
 
 
---
+++ project1/scripts/implementations.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""regression functions for project 1."""
+
+
+#NB : all functions should return (weight, loss)
+#NB : we are only interested in the last one 
+# il manque logistic regression using gradient descent (or SGD)  et reg logistic regression
+
+import numpy as np
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """Gradient descent algorithm with mse."""
+    # Linear regression using gradient descent 
+    # Define parameters to store w and loss
+    loss = 0
+    w = initial_w
+    for n_iter in range(max_iters):
+        # ***************************************************
+        grad = compute_gradient(y, tx, w)
+        loss = compute_loss(y, tx, w, method="mse")
+        w = w - gamma*grad
+        # ***************************************************
+        if not(n_iter%30):
+            print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return w, loss
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma, batch_size=1):
+    """Stochastic gradient descent algorithm."""
+    # ***************************************************
+    grad = 0
+    loss = 0
+    w = initial_w
+    for n_iter in range(max_iters):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
+                grad = compute_gradient(minibatch_y, minibatch_tx,w)
+        grad = grad / batch_size    
+        w = w - grad * gamma
+        loss = compute_loss(y, tx, w)
+        if not(n_iter%10):
+            print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+                  bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    # ***************************************************
+    return w, loss
+
+def least_squares(y, tx):
+    """calculate the least squares solution."""
+    #least squares regression using normal equations
+    # ***************************************************
+
+    w=np.linalg.solve(np.dot(tx.T,tx),np.dot(tx.T,y))
+    lmse= compute_loss(y, tx, w)
+    return w, lmse
+    # ***************************************************
+    
+def ridge_regression(y, tx, lambda_):
+    #ridge regression using normal equations
+    # ***************************************************
+    w=np.linalg.solve((np.dot(tx.T,tx)+lambda_*2*y.shape[0]*np.eye(tx.shape[1])),np.dot(tx.T,y))
+    lmse= compute_loss(y, tx, w)
+    # ***************************************************
+    return w,lmse
+
+## A RENOMMER ET INTERVERTIR L ET W AVEC LE CODE ECRIT EN MEME TEMPS :logistic_regression(y, tx, initial w, max iters, gamma)
+## N'UTILISE PAS GAMMA...
+def find_optimal_w(y, tX, w_initial, max_iters):
+    """Finds the most likely weights using the iterative Newton-Raphson method."""
+    
+    # Define parameters to store w and loss
+    ws = [w_initial]
+    log_likelihoods = [compute_log_likelihood(w_initial,tX, y)]
+    w = w_initial
+    for n_iter in range(max_iters):
+        
+        with np.errstate(divide='raise',invalid='raise'):
+            try:
+                gradient = compute_gradient_log_likelihood(w, tX, y)
+                jacobean = compute_jacobean_log_likelihood(w,tX, y)
+            except FloatingPointError as e:
+                print(e)
+                print(tX, n_iter)
+                return log_likelihoods, ws
+        
+        try:
+            w = w - np.linalg.solve(jacobean,gradient)
+        except np.linalg.LinAlgError as e:
+            print(e)
+            #print(log_likelihood,gradient,jacobean)
+            print(tX, n_iter)
+            
+            return log_likelihoods, ws
+            
+        
+        # store w and loss
+        ws.append(w)
+        log_likelihood = compute_log_likelihood(w,tX, y)
+        log_likelihoods.append(log_likelihood)
+
+
+    return log_likelihoods, ws
